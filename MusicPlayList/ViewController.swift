de1d6c81f58d892f717153914557f1a27f50ce57
Maninder Jheeta <manindersinghjheeta@Maninders-MacBook-Air.local>
2017-07-09 15:47:34 -0400

    Initial Commit

---
+++ MusicPlayList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayList
+//
+//  Created by Maninder Singh Jheeta on 7/9/17.
+//  Copyright © 2017 Maninder Singh Jheeta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
